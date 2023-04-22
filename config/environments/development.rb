de2923f9bb6385df2dff427f3d9013c716cf2ee3
HungDV <hungdv4>
2023-04-22 23:15:30 +0700

    add gem devise and setup stmp mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,6 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
   # Enable server timing
   config.server_timing = true
 
@@ -67,4 +66,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:  "doviethung271200@gmail.com",
+    password: 'hrtgodqyjwetatak',
+    domain: 'localhost:3000',
+    address:  'smtp.gmail.com',
+    port: '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
