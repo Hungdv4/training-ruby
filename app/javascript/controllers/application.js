3bd15b8e0249455aa017b9e75162e7135b62dc30
HungDV <hungdv4>
2023-04-24 14:31:05 +0700

    css view signin signup and create table post

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,4 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "stimulus"
 
 const application = Application.start()
 
@@ -7,3 +7,13 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
+application.register('autosave', Autosave)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
+application.register('slideover', Slideover)
